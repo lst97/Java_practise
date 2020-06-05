d58aada33f21fa02775c09dad0d02bc44f915e5d
SIO TOU LAI (Nelson) <laisiotou1997@gmail.com>
2020-06-05 15:27:04 +1000

    Create Achievement.java

---
+++ src/com/Achievement.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Achievement {
+    public String description;
+    public int level;
+    public int maximum;
+
+    public Achievement(String description, int level, int maximum){
+        this.description = description;
+        this.level = level;
+        this.maximum = maximum;
+    }
+}
